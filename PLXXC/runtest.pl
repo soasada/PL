b94b4f6eca0ba26245283b0cc433b7d4630d5346
Nicolás Vargas Ortega <nvortega92@gmail.com>
2015-09-02 13:38:15 +0200

    Implemented runtest with Perl

---
+++ PLXXC/runtest.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+
+use strict;
+#use warnings;
+#use diagnostics;
+#use autodie;
+
+my $plxcFile;
+my $ctdFile;
+
+my $cont = 0;
+my $cont2 = 0;
+
+my @files = </home/z0rtecx/Downloads/plx-test/*>;
+foreach $plxcFile (@files){
+	if($plxcFile =~ /\.plx$/i){
+		$ctdFile = $plxcFile;
+		$ctdFile =~ s/\.plx/\.ctd/;
+
+		my $compila = qx(./plxc $plxcFile $ctdFile);
+		my $salida1 = qx(./ctd $ctdFile);
+		
+		my $compila2 = qx(java PLXC $plxcFile $ctdFile);
+		my $salida2 = qx(./ctd $ctdFile);
+
+		if($salida1 eq $salida2){
+			$cont++;
+		}
+		
+		$cont2++;
+
+	}
+}
+
+if($cont == $cont2){
+	print "Éxito!\n";
+}
+else{
+	print "Has tenido algún error, número de test pasados: " . $cont . " de " . $cont2 . "\n";
+}
